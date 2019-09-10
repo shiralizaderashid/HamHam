451e9c3d3d9f586143947d685ec053fdb7289b1c
DerSkythe <>
2019-09-10 16:52:38 +0400

    First commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        transform.Translate(UnityEngine.Vector2.left * speed * Time.deltaTime); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (other.tag == gameObject.tag)
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float startTimeBtwSpawn;
+
+    private float timeBtw;
+
+    public GameObject[] enemies;
+
+    private void Update()
+    {
+        if (timeBtw <= 0)
+        {
+            int rand = Random.Range(0, enemies.Length);
+            Instantiate(enemies[rand], transform.position, Quaternion.identity);
+            timeBtw = startTimeBtwSpawn;
+        }
+        else
+        {
+            timeBtw -= Time.deltaTime;
+        }
+    }
+}
